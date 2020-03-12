799ae25651eca9e06cf41989ae2a168454af3de4
Gabriel Hayes <gabriel.speed.bullock@gmail.com>
2020-03-11 19:26:07 -0500

    IPC works! Yay

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -6,39 +6,72 @@ import tink.unit.Assert.assert;
 import synch.SynchLib;
 import sys.FileSystem;
 import Utils.attempt;
+using Utils;
 
 using tink.CoreApi;
 using Lambda;
 
 class RunTests {
 	static function main() {
-		Runner.run(TestBatch.make([new BasicTest()])).handle(Runner.exit);
+		Runner.run(TestBatch.make([new NativeTest()])).handle(Runner.exit);
 	}
 }
 
 
-class BasicTest {
+class NativeTest {
     public function new() {}
     var handle:SynchronizationHandle;
+    var eventId:String;
+    var asserts = new AssertionBuffer();
     public function test_create_event() {
-        handle = synch.SynchLib.create_event("Local\\" + Std.string(Std.random(10000)));
-        return assert(handle != null);
+        return assert(attempt({
+            #if master
+            eventId = Std.string(Std.random(10000));
+            handle = synch.SynchLib.create_event("Local\\" + eventId);
+            #elseif slave
+            eventId = Sys.stdin().readLine();
+            handle = synch.SynchLib.open_event("Local\\" + eventId);
+            #end
+        }));
     }
+    #if master
     public function wait_handle() {
-        final now = Date.now();
-        trace('Start: $now');
-        Thread.create(() -> {
-            Sys.sleep(5);
-            trace('Signaling event@${Date.now()}');
+        return Utils.shouldLast({
+            Thread.create(() -> {
+                Sys.sleep(5);
+                trace('Signaling event@${Date.now()}');
+                handle.signal_event();
+            });
+            handle.wait_for_handle(10 * 1000);
+        }, 5000, 10);
+    }
+    #end
+    var slave:sys.io.Process;
+    var liStart = '>>>>>>>>';
+    public function test_ipc_event() {
+        #if slave
+        return Utils.shouldLast({
+            handle.wait_for_handle(10 * 1000);
+            Sys.println('Closing slave');
+            Sys.sleep(2.5);
+            // Sys.
+        }, 5000, 10);
+        #elseif master
+        handle.reset_event();
+        return Utils.shouldLast({
+            slave = new sys.io.Process('hl slave.sample.hl');
+            slave.stdin.writeString('$eventId\r\n');
+            Sys.sleep(2.5);
             handle.signal_event();
-        });
-        handle.wait_for_handle(10 * 1000);
-        final then = Date.now();
-        final delta = (then.getTime() - now.getTime());
-        final expected = 5000;
-        final threshhold = 10;
-        trace('Delta/Expected: $delta/$expected');
-        trace('End: $then');
-        return assert(delta > expected - threshhold && delta < expected + threshhold);
+            final output = slave.stdout.readAll().toString();
+            Sys.println('$liStart SLAVE OUTPUT:\r\n' + ~/($)/mg.replace(output, '$1$liStart\t\t'));
+            final exitCode = slave.exitCode(true);
+            asserts.assert(output.indexOf('Closing slave') != -1);
+            asserts.assert(output.indexOf('0 Failure') != -1);
+            asserts.assert(exitCode == 0);
+        }, 5000, 25, asserts);
+        #else
+            #error
+        #end
     }
 }
--- tests/Utils.hx
+++ tests/Utils.hx
@@ -1,5 +1,9 @@
+import haxe.macro.Expr;
+import tink.testrunner.*;
+import tink.unit.*;
+import tink.unit.Assert.assert;
 class Utils {
-    public static macro function  attempt(expr:haxe.macro.Expr){
+    public static macro function  attempt(expr:Expr){
 
         return macro try {
             $expr;
@@ -9,5 +13,22 @@ class Utils {
             false;
         };
     
-} 
+    }
+    public static macro function shouldLast(expr:Expr, expected:ExprOf<Int>, threshhold:ExprOf<Int>, ?asserts:ExprOf<tink.unit.AssertionBuffer>){
+        return macro {
+            final then = Date.now();
+            trace('Start:' + then);
+            $expr;
+            final now = Date.now();
+            final delta = (now.getTime() - then.getTime());
+            trace('Delta/Expected: ' +  Std.string(delta) + "/" + $expected);
+            trace('End:' +  now);
+            var _a = $asserts;
+            if(_a == null)
+                _a = new AssertionBuffer();
+            _a.assert(delta > $expected - $threshhold && delta < $expected + $threshhold);
+            _a.done();
+            return _a;
+        };
+    }
 }
