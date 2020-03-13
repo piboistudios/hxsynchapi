0a41daf9bd67c1e1f7f88c488d9fd1983dc1660e
Gabriel Hayes <gabriel.speed.bullock@gmail.com>
2020-03-13 17:42:34 -0500

    ipc mutex, synch barrier tests

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -34,7 +34,8 @@ class NativeTest {
 	var handle:SynchronizationHandle;
 	var eventId:String;
 	var asserts = new AssertionBuffer();
-
+    inline function printSlaveOutput(output)
+        Sys.println('$liStart SLAVE OUTPUT:\r\n' + ~/($)/mg.replace(output, '$1$liStart\t\t'));
 	#if (master || slave)
 	public function test_create_event() {
 		return assert(attempt({
@@ -55,12 +56,12 @@ class NativeTest {
 	public function event_wait_handle() {
 		return Utils.shouldLast({
 			Thread.create(() -> {
-				Sys.sleep(5);
+				Sys.sleep(1);
 				trace('Signaling event@${Date.now()}');
 				handle.event_signal();
 			});
 			handle.synch_wait_for_handle(10 * 1000);
-		}, 5000, 10);
+		}, 1000, 10);
 	}
 	#end
 
@@ -77,20 +78,19 @@ class NativeTest {
 			sys.thread.Thread.create(() -> {
 				Sys.println('Event triggered');
 				Sys.println('Closing slave');
-				Sys.println('Sleeping 1 seconds');
+				Sys.println('Sleeping 1 second');
 			});
 			Sys.sleep(1);
 			// Sys.
-		}, 2000, 1000);
+		}, 2000, 100);
 		#elseif master
 		handle.event_reset();
 		return Utils.shouldLast({
 			// asserts = new AssertionBuffer();
 			slave = new sys.io.Process('hl slave.sample.hl');
 			slave.stdin.writeString('$eventId\r\n');
-			inline function printSlaveOutput(output)
-				Sys.println('$liStart SLAVE OUTPUT:\r\n' + ~/($)/mg.replace(output, '$1$liStart\t\t'));
-			Sys.println('Sent $eventId to slave, waiting 1 seconds');
+			
+			Sys.println('Sent $eventId to slave, waiting 1 second');
 			final initialOutput = slave.stdout.readUntil('%'.charCodeAt(0)).toString();
 			printSlaveOutput(initialOutput);
 
@@ -124,7 +124,7 @@ class NativeTest {
 	public function test_open_mutex() {
 		return assert(attempt({
 			this.mutexId = Sys.stdin().readLine();
-			this.mutex = synch.SynchLib.mutex_open('Global\\$mutexId', true);
+			this.mutex = synch.SynchLib.mutex_open('Global\\$mutexId', false);
 			trace('my mutex: $mutex ($mutexId)');
 		}));
 	}
@@ -149,7 +149,35 @@ class NativeTest {
 			mutex.mutex_release();
 		}, 2000, 1000);
 	}
-	#end
+    #end
+    #if (master||slave2)
+    public function test_ipc_mutex() {
+        #if slave2
+        trace('Waiting for handle%');
+        mutex.synch_wait_for_handle(10 * 1000);
+        final content = sys.io.File.getContent('./data.txt');
+        sys.io.File.saveContent('./data.txt', content + content);
+        mutex.mutex_release();
+        return assert(sys.io.File.getContent('./data.txt') == 'some data\r\nsome data\r\n');
+        #elseif master
+        mutex.synch_wait_for_handle(10 * 1000);
+        sys.io.File.saveContent('./data.txt', 'some data\r\n');
+        final slave = new sys.io.Process('hl slave2.sample.hl');
+        slave.stdin.writeString('$mutexId\r\n');
+        slave.stdin.flush();
+        printSlaveOutput(slave.stdout.readUntil('%'.charCodeAt(0)));
+        trace('releasing mutex');
+        mutex.mutex_release();
+        mutex.synch_wait_for_handle(10 * 1000);
+        asserts.assert(sys.io.File.getContent('./data.txt') == 'some data\r\nsome data\r\n');
+        mutex.mutex_release();
+        printSlaveOutput(slave.stdout.readAll().toString());
+        asserts.assert(slave.exitCode(true) == 0);
+        return asserts.done();
+        #end
+    }
+    #end
+
 
 	var criticalSection:CriticalSection;
 
@@ -171,7 +199,7 @@ class NativeTest {
 			criticalSection.critical_section_enter();
 			threadMsg("Entering crtiical section. Doing work.");
 			criticalValue+= 10;
-			Sys.sleep(1);
+			Sys.sleep(0.1);
 			done++;
 			a.assert(criticalValue == done * 10);
 			threadMsg("Leaving critical section. Work done. done: " + done);
@@ -179,7 +207,6 @@ class NativeTest {
 			if (done == 10) {
 				a.assert(criticalValue == 100);
 				a.done();
-				trace('ASSERTS DONE');
 			}
 		});
 	}
@@ -197,5 +224,45 @@ class NativeTest {
 			criticalSection.critical_section_delete();
 		}));
 	}
-	#end
+    #end
+    #if master
+    var barrier:SynchronizationBarrier;
+    public function test_create_barrier() {
+        return assert(attempt({
+            this.barrier = synch.SynchLib.synch_barrier_init(10, SPIN_COUNT);
+        }));
+    }
+    public function test_synch_barrier() {
+        var counter = 0;
+        inline function dispatch(delay:Int)
+            sys.thread.Thread.create(() -> {
+                inline function msg(msg) trace('$liStart Thread $delay: $msg');
+                msg('Waiting ${delay/10}s');
+                Sys.sleep(delay/10);
+         
+                msg('Incrementing counter');
+                counter++;
+                msg('Entering barrier');
+                barrier.synch_barrier_enter(false, false);
+                msg('Passing barrier');
+                if(delay == 0) asserts.assert(counter == 10);
+                Sys.sleep(delay/10);
+                msg('Decrementing counter');
+                counter--;
+                if(delay == 9) {
+                    msg('Last out. Cleaning up');
+                    barrier.synch_barrier_delete();
+                    asserts.assert(counter == 0);
+                    asserts.done();
+                }
+                
+            });
+        for(i in 0...10) {
+            dispatch(i);
+        }
+        return asserts;
+
+        
+    }
+    #end
 }
